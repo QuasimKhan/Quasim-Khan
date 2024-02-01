ff66b98b6e9311ebd2591f3260817db6a3331bde
QuasimKhan <quasimkhanpersonal@gmail.com>
2024-02-01 20:27:05 +0530

    Changes

--- src/app.js
+++ src/app.js
@@ -3,20 +3,15 @@ const menu = document.querySelector("#menu")
 const moon = document.querySelector("#moon")
 const body = document.querySelector("body")
 const hLinks = document.querySelectorAll("#hLink")
-const crossI = document.querySelector("#crossI")
-const threeLine1 = document.querySelector("#three-line1")
-const threeLine2 = document.querySelector("#three-line2")
-const threeLine3 = document.querySelector("#three-line3")
+
+
 
 
 
 hamburger.addEventListener("click", ()=>{
   menu.classList.toggle("hidden")
-  hamburger.classList.toggle("text-white")
-  crossI.classList.toggle("hidden")
-  threeLine1.classList.toggle("hidden")
-  threeLine2.classList.toggle("hidden")
-  threeLine3.classList.toggle("hidden")
+  hamburger.classList.toggle("bg-white")
+ 
 })
 
 hLinks.forEach(link=>{
